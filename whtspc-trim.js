c41f5616c981edc384ab7210e4e202506266c0b7
Nisrine Jednane <34342007+NisrineJednane@users.noreply.github.com>
2018-06-10 22:02:28 +0000

    Create whtspc-trim.js

---
+++ whtspc-trim.js
@@ -0,0 +1,12 @@
+function trimIt (str) {
+
+  var result;
+  if (typeof str === 'string') {
+      result = str.replace(/^\s+|\s+$/g, "");
+      
+      return result;
+    
+    } else {
+      return false;
+  }
+}
