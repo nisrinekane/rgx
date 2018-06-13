fa2b0334475dc0b4b812bd36b061d2c1b22e1f2b
Nisrine Jednane <34342007+NisrineJednane@users.noreply.github.com>
2018-06-13 23:44:59 +0000

    Update whtspc-trim.js

--- whtspc-trim.js
+++ whtspc-trim.js
@@ -1,11 +1,10 @@
+//trim whitespace
 function trimIt (str) {
-
+  
   var result;
   if (typeof str === 'string') {
       result = str.replace(/^\s+|\s+$/g, "");
-      
       return result;
-    
     } else {
       return false;
   }
